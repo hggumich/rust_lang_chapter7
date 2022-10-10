242a3cb736e6d1eba64e38423ff8dac5693e8f35
Hector Garza <hggumich@gmail.com>
2022-10-10 18:19:37 -0400

    start restaurant program

---
+++ restaurant/target/debug/deps/restaurant-13616d0cd9a30876.d
@@ -0,0 +1,5 @@
+c:\Users\hectorg\Desktop\projects\rust_lang_chapter7\restaurant\target\debug\deps\restaurant-13616d0cd9a30876.rmeta: src\lib.rs
+
+c:\Users\hectorg\Desktop\projects\rust_lang_chapter7\restaurant\target\debug\deps\restaurant-13616d0cd9a30876.d: src\lib.rs
+
+src\lib.rs:
---
+++ restaurant/target/debug/deps/restaurant-e6bc60506439fe63.d
@@ -0,0 +1,5 @@
+c:\Users\hectorg\Desktop\projects\rust_lang_chapter7\restaurant\target\debug\deps\restaurant-e6bc60506439fe63.rmeta: src\lib.rs
+
+c:\Users\hectorg\Desktop\projects\rust_lang_chapter7\restaurant\target\debug\deps\restaurant-e6bc60506439fe63.d: src\lib.rs
+
+src\lib.rs:
