a3ffffd1fe13bbe5bc2b36b44eb1939ee56537d3
Hector Garza <hggumich@gmail.com>
2022-10-11 17:22:04 -0400

    add relative paths with super

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -5,18 +5,28 @@ mod front_of_house {
         fn seat_at_table() {}
     }
 }
-    mod serving {
-        fn take_order() {}
+mod serving {
+    fn take_order() {}
 
-        fn serve_order() {}
+    fn serve_order() {}
 
-        fn take_payment() {}
+    fn take_payment() {}
+}
+
+pub fn eat_at_restaurant() {
+    //Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    //Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
     }
-
-    pub fn eat_at_restaurant() {
-        //Absolute path
-        crate::front_of_house::hosting::add_to_waitlist();
-
-        //Relative path
-        front_of_house::hosting::add_to_waitlist();
-    }
+    fn cook_order() {}
+}
